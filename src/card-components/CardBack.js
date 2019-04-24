387d0b080afbda004a0fc285ab2ffd52dc2b268f
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-24 18:14:44 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -22,7 +22,7 @@ export default class CardBack extends Component {
       <div className="card-back">
         <h3 className="title">{this.props.title}</h3>
         <span />
-        {this.generateRatingElement()}
+          {this.generateRatingElement()}
         <span />
         <h5 className="genres">{this.props.genres.join(', ')}</h5>
       </div>
